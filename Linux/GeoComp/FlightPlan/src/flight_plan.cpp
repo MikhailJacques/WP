594f10fb342221f5dbfb4be899b87b6264a86e29
user <user@optiplex.com>
2022-05-31 14:24:52 +0300

    Forteenth

--- Linux/GeoComp/FlightPlan/src/flight_plan.cpp
+++ Linux/GeoComp/FlightPlan/src/flight_plan.cpp
@@ -298,7 +298,7 @@ STATE_DEFINE(FlightPlan, Calculate_Drone_Scan_Route_State, NoEventData)
 
 	::rti::core::bounded_sequence<dds_msgs::GeoPoint, 100L> geo_points;
 
-	for (int jj = 0; jj < 8; jj++)
+    for (int jj = 0; jj < 100; jj++)
 	{
 		geo_points.push_back(flight_path[jj]);
 	}
@@ -318,7 +318,7 @@ STATE_DEFINE(FlightPlan, Calculate_Drone_Scan_Route_State, NoEventData)
 	// Compose message
 	m_drone_scan_route_msg.MsgId(3);
 	m_drone_scan_route_msg.MsgCount(++m_drone_scan_route_msg_cnt);		// 200
-	m_drone_scan_route_msg.MissionId(150);								// m_get_drone_scan_route_msg.MissionId()
+    m_drone_scan_route_msg.MissionId(m_get_drone_scan_route_msg.MissionId());
 	m_drone_scan_route_msg.Waypoints(geo_points);
 
 	// MJ TODO: Discuss with Hai the absense of need in these parameters in light of the new circular-path navigation algorithm
